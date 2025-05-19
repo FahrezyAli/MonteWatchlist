696d7cf2f967058f32d808139d2ec77932d16458
Ali Ahmad Fahrezy <47922563+FahrezyAli@users.noreply.github.com>
2025-05-20 01:10:48 +0700

    SearchBar component now support onSubmit callback

--- MonteWatchlist/Components/SearchBar.swift
+++ MonteWatchlist/Components/SearchBar.swift
@@ -3,6 +3,7 @@ import SwiftUI
 // Search bar component
 struct SearchBar: View {
     @Binding var text: String
+    var onSubmit: (() -> Void)?
 
     var body: some View {
         HStack {
@@ -11,6 +12,9 @@ struct SearchBar: View {
                 .padding(.horizontal, 25)
                 .background(Color(.systemGray6))
                 .cornerRadius(8)
+                .onSubmit {
+                    onSubmit?()
+                }
                 .overlay(
                     HStack {
                         Image(systemName: "magnifyingglass")
